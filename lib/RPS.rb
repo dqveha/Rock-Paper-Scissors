fefd05167a76a791199f2942eb4f5187eccfca58
Gengur123 <meleearthur@gmail.com>
2021-07-14 09:58:48 -0700

    Added third test
    co-authored-by: Andrew Giang <giang184@gmail.com>
    co-authored-by: Dave Lindqvist <lindqvist.dave@gmail.com>

--- lib/RPS.rb
+++ lib/RPS.rb
@@ -5,6 +5,8 @@ class RPS
   def wins?(player1, player2)
     if (player1 === "rock") && (player2 === "scissors")
       true
+    elsif (player1 === "scissors") && (player2 === "paper")
+      true
     else
       false
     end
--- spec/RPS_spec.rb
+++ spec/RPS_spec.rb
@@ -12,4 +12,9 @@ describe('RPS#wins?') do
     game = RPS.new()
     expect(game.wins?("rock", "rock")).to(eq(false))
   end
+
+  it("returns true if scissors is the object and paper is the argument") do
+    game = RPS.new()
+    expect(game.wins?("scissors", "paper")).to(eq(true))
+  end
 end
