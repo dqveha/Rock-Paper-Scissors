4872b305328d6137845e0dc3c61752c87dab3b33
Gengur123 <meleearthur@gmail.com>
2021-07-14 11:27:21 -0700

    Added scripts
    co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>
    co-authored-by: Kristen Hopper <hopperdavis@gmail.com>
    co-authored-by: Andrew Giang <giang184@gmail.com>
    co-authored-by: Dave Lindqvist <lindqvist.dave@gmail.com>

--- lib/script.rb
+++ lib/script.rb
@@ -1,30 +1,25 @@
 require_relative 'RPS'
 game = RPS.new()
 
-player1 = game.random_rsp
-player2 = game.random_rsp
-puts player1
-puts player2
-puts game.wins?(player1,player2)
+puts 'enter the number of games you want to play'
+num_game = gets.chomp
+# puts num_game
 
-player1 = game.random_rsp
-player2 = game.random_rsp
-puts player1
-puts player2
-puts game.wins?(player1,player2)
-
-player1 = game.random_rsp
-player2 = game.random_rsp
-puts player1
-puts player2
-puts game.wins?(player1,player2)
-
-player1 = game.random_rsp
-player2 = game.random_rsp
-puts player1
-puts player2
-puts game.wins?(player1,player2)
-
-puts game.total_wins
-puts game.total_losses
-puts game.total_ties
+num_game.to_i.times {
+  puts 'enter r, p, or s'
+  player1 = gets.chomp
+  if (player1 === "r")
+    player1 = "rock"
+  elsif (player1 === "p")
+    player1 = "paper"
+  else
+    player1 = "scissors"
+  end
+  player2 = game.random_rsp
+  puts player1
+  puts player2
+  game.wins?(player1,player2)
+  puts "wins: #{game.total_wins}"
+  puts "losses: #{game.total_losses}"
+  puts "ties: #{game.total_ties}"
+}
